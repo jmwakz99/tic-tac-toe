e36c4c2a77506a0855cbfd86b861e9662c4fddc3
Johnson Mwakazi <mwanjilajohnson@gmail.com>
2020-07-18 11:53:37 +0300

    Updates

---
+++ precache-manifest.b56620d5c09ab63be0faae942d803bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4021cbcc863eed4a617ccec52dbcbdd5",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "44ef362e61fd63ee54af",
+    "url": "/tic-tac-toe/static/css/main.dd75a44f.chunk.css"
+  },
+  {
+    "revision": "1fa823baadcc7c797c43",
+    "url": "/tic-tac-toe/static/js/2.e234924a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.e234924a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ef362e61fd63ee54af",
+    "url": "/tic-tac-toe/static/js/main.e1c49c9d.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
